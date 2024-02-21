840d8d29ce3f2592707bc02318b8ce45904df354
larmitage5 <146672196+larmitage5@users.noreply.github.com>
2024-02-21 17:18:43 +0000

    setup

---
+++ code.c
@@ -0,0 +1,41 @@
+/* include 
+struct definitions
+function/procedure definitions
+basic main outline
+use comments to give basic idea of code structure
+*/
+
+/*
+required functions
+- move up
+- move down
+- move left
+- move right
+- show map
+- player position
+*/
+
+
+/* 
+required structs
+- 
+*/
+
+// struct for position of player
+
+typedef struct {
+    // 2d array??
+    char position[100][100];
+} position;
+
+int move (){
+
+}
+
+int map() {
+
+}
+
+int end() {
+    
+}
--- test.sh
+++ test.sh
@@ -1,1 +1,13 @@
-
+# things that need to be checked:
+# start at S?
+# end at E?
+# correct message displayed at E
+# map only showed when m is pressed
+# error message if player moves into wall
+# error message if player tries to move off map
+# key other than wasdm inputted
+# works for both capitalised and uncapitalised
+# is it solvable???
+# test for success
+# Each test should have a comment above it stating what it is testing for
+# executable will be names maze
