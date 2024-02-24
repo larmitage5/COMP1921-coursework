50926b49960cb1f1053bac4807286f195a341ec2
larmitage5 <146672196+larmitage5@users.noreply.github.com>
2024-02-24 16:29:35 +0000

    add

--- code.c
+++ code.c
@@ -13,6 +13,7 @@ required functions
 - move right
 - show map
 - player position
+- check maze file
 */
 
 
@@ -25,17 +26,89 @@ required structs
 
 typedef struct {
     // 2d array??
-    char position[100][100];
+    char row[100];
+    char pos[100][100];
 } position;
 
-int move (){
+void readIn (){
+    /* reads in text file
+    adds each row to an array
+    for each row, adds each character to an array, creating a 2d array for position
+    prints to new text file called map.txt
+    */
+}
 
+int checkMaze (){
+    /* checks that maze file is valid
+    checks that each item in the array has the same length - else displays 'Invalid Maze'
+    checks that each array only contains ' ','#','S','E' - else displays 'Invalid Maze: invalid characters'
+    creates a counter to check that across all arrays, there is only one 'S' and only one 'E'
+    - else displays 'Invalid Maze: multiple starts' or 'Invalid Maze: multiple ends'
+    */
+}
+
+int moveUp (){
+    /* checks the item in the array of the row above in the same position
+      for pos[n][8] checks pos[n-1][8]
+      if it is '#' displays error message
+      if it is 'E' calls the end() function
+      else, value of pos[n-1][8] becomes 'X' and value of pos[n][8] becomes ' '
+      updates text file accordingly
+      displays 'Successfully moved up.'
+    */
+}
+
+int moveDown(){
+    /* checks the item in the array of the row below in the same position
+      for pos[n][8] checks pos[n+1][8]
+      if it is '#' displays error message
+      if it is 'E' calls the end() function
+      else, value of pos[n+1][8] becomes 'X' and value of pos[n][8] becomes ' '
+      updates text file accordingly
+      displays 'Successfully moved down.'
+    */
+}
+
+int moveRight(){
+    /* checks the next item in the same array
+      for pos[n][8] checks pos[n][9]
+      if it is '#' displays error message
+      if it is E calls the end() function
+      else, value of pos[n][9] becomes 'X' and value of pos[n][8] becomes ' '
+      updates text file accordingly
+      displays 'Successfully moved right.'
+    */
+}
+
+int moveLeft(){
+    /* checks the previous item in the same array
+      for pos[n][8] checks pos[n][7]
+      if it is '#' displays error message
+      if it is E calls the end() function
+      else, value of pos[n][7] becomes 'X' and value of pos[n][8] becomes ' '
+      updates text file accordingly
+      displays 'Successfully moved left.'
+    */
 }
 
 int map() {
+    /* prints map.txt
+    */
+}
 
+int start() {
+    // replaces 'S' in map.txt with 'X'
 }
 
 int end() {
+    // displays message 'Congratulations, you have exited the maze!'
+    // exits the programme
+}
+
+
+int main () {
+    readIn(filename);
+    checkMaze(position);
+
     
 }
--- test.sh
+++ test.sh
@@ -11,3 +11,46 @@
 # test for success
 # Each test should have a comment above it stating what it is testing for
 # executable will be names maze
+
+
+# TEST FOR USER INPUTS
+# wrong key entered, too many keys entered, lowercase/uppercase, nothing entered, bad file, bad permissions, 
+
+
+# TEST MAZE FILES
+# all columns and rows same length, one S, one E, only the 4 specified characters,
+
+# SUCCESS
+# up, down, left, right work correctly, starts at S, ends at E, m displays map, maze successfully loaded
+
+
+#!/bin/bash
+
+echo -e "~~ File Tests ~~"
+
+echo -n "Testing bad maze dimensions - "
+echo -n "Testing multiple starts - "
+echo -n "Testing multiple ends - "
+echo -n "Testing invalid maze characters - "
+echo -n "Testing bad filename - "
+echo -n "Testing bad permissions - "
+
+echo -e "\n~~ User Input Tests ~~"
+
+echo -n "Testing for no user input - "
+echo -n "Testing for invalid user input - "
+echo -n "Testing for multiple user inputs - "
+echo -n "Testing for lowercase user input - "
+
+echo -e "\n~~ Success Tests ~~"
+
+echo -n "Testing for maze loading successfully - "
+echo -n "Testing for moving up - "
+echo -n "Testing for moving down - "
+echo -n "Testing for moving right - "
+echo -n "Testing for moving left - "
+echo -n "Testing for map loading - "
+echo -n "Testing for correct end - "
+
+
+rm -f tmp
